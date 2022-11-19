1cb5c0642af18bd9f9e530a2302ec98ee5c79e18
w0l1d <ahdoufwalid@gmail.com>
2022-11-19 05:05:22 -0600

    fix wait

--- TP4/ex1b.c
+++ TP4/ex1b.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <signal.h>
 
@@ -12,13 +13,21 @@ et le code nécessaire afin que le père n’attende jamais son fils de façon b
 que le fils ne devienne pas zombie.
 
 */
+int status = -1;
+
+void* handler() {
+	wait(&status);
+	printf("child is here\n");
+}
+
+
 
 
 /*0*/
 int main(int argc, char *argv[])
 {
-	if (signal(SIGCHLD, SIG_IGN) == SIG_ERR) {
-		perror("error : connot associate handler ignore to child signal");
+	if (signal(SIGCHLD, &handler) == SIG_ERR) {
+		perror("error : connot associate handler ignore to child signal\n");
 		exit(1);
 	}
 	
@@ -27,13 +36,20 @@ int main(int argc, char *argv[])
 		/*2*/
 		for (int i = 0 ; i <10 ; i++, sleep(1), printf("calculating..\n")) ; //simule un petit calcul
 		/*3*/
-		exit(1);
+		exit(100);
 		/*4*/
 	}
 	
 	/*5*/
 	int i = 0;
-	while(i++ < 3) { usleep(500000); printf("waiting...\n"); } //Simule un calcul infini
+	while(i++ < 100) { 
+		if (status != -1) {
+			printf ("\nchild exit status : %d", WEXITSTATUS(status));
+			status = -1;
+		} 
+		usleep(500000);
+		printf("waiting...\n"); 
+	} //Simule un calcul infini
 	/*6*/
 }
 
